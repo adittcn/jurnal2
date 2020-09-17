f6b0733475f563359a6486be8a121dacb383d690
adittcn <adittcn18@gmail.com>
2020-09-17 16:25:24 +0700

    Jurnal.java

---
+++ praktikum/Jurnal.java
@@ -0,0 +1,8 @@
+public class Jurnal{
+	public static void main(String[] args){
+		System.out.println("Nama : Aditya lesmana");
+		System.out.println("NIM : 6706194060");
+		System.out.println("Kelas : 43-03");
+
+	}
+}
